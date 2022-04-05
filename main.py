c5a7a210133610dfdfb6d7003a7f5f9873a52675
kin0man_ <ilakinsasha2023@gmail.com>
2022-04-05 20:09:52 +0300

    Первый день готов - готова первая функция 😎

---
+++ main.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+from discord import File
+import requests
+
+
+bot = commands.Bot(command_prefix='-')
+
+
+@bot.command(name='place')
+async def timer(ctx, *args):
+    longitude = 47.25
+    lattitude = 56.1
+    if len(args) > 1:
+        longitude = args[0]
+        lattitude = args[1]
+    map_request = f"https://static-maps.yandex.ru/1.x/?ll={longitude},{lattitude}&size=650,450&z=11&l=map"
+    response = requests.get(map_request)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    await ctx.send('Вы здесь:')
+    await ctx.send(file=File('map.png'))
+
+
+bot.run('OTYwOTMyNTkzNzQwNjkzNTQ0.YkxoNw.vm2-78Tvy_1UGI-W17ZiGok2vs8')
